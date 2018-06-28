b6fc4e9896001ea9623187eebdf21a6ff774fac6
Joaquin Ronda <joaronbe@teleco.upv.es>
2018-06-28 13:42:42 +0200

    gps funcionando!

--- mobile/src/main/java/com/es/upv/etsit/aatt/botonsocorro/MainActivity.java
+++ mobile/src/main/java/com/es/upv/etsit/aatt/botonsocorro/MainActivity.java
@@ -19,6 +19,9 @@ import android.widget.TextView;
 import android.net.Uri;
 import android.util.Log;
 import android.content.pm.PackageManager;
+import android.location.*;
+import java.io.*;
+import java.util.*;
 
 
 // nuevas
@@ -45,12 +48,21 @@ public class MainActivity extends AppCompatActivity {
     String telefono;
     String email;
     boolean preferenciasGuardadas;
+    String latitud = "nose";
+    String longitud = "nose";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //pide permiso gps y pide la localización
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+            locationStart();
+        }
+
 
 // FUNCION ESPERA =========================================================================================================================================================
         //aqui irá el hilo en segundo plano que tendra que estar esperando la posible pulsación del reloj
@@ -131,8 +143,7 @@ public class MainActivity extends AppCompatActivity {
     // FUNCION SOS
     public void sos() {
 
-        final String latitud = "nose";
-        String longitud = "nose";
+
 
         //Paso 1.      CARGAMOS LOS DATOS
         SharedPreferences prefs = getSharedPreferences("MisDatosApp", Context.MODE_PRIVATE);
@@ -151,6 +162,8 @@ public class MainActivity extends AppCompatActivity {
         //Intent intencionLlamar = new Intent(Intent.ACTION_DIAL, Uri.parse(telefono));
 
 
+
+
         // ENVIAMOS SMS ===============================================================================================================================================================
  /*       if(ActivityCompat.checkSelfPermission(
                 MainActivity.this, Manifest.permission.SEND_SMS)
@@ -163,7 +176,6 @@ public class MainActivity extends AppCompatActivity {
             enviarMensaje(telefono,nombre+ " ha pulsado SOS"); // Hay una función auxiliar que manda el sms.
         }
 */
-
 
 
         // ENVIAMOS EMAIL DE SOS
@@ -221,41 +233,140 @@ public class MainActivity extends AppCompatActivity {
 */
 
 
+        //SIGUE EMAIL ========================================
 
-            //SIGUE EMAIL ========================================
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Atención! " + nombre + " ha pulsado el botón en: " + latitud + " y " + longitud + ".");
 
-            emailIntent.putExtra(Intent.EXTRA_TEXT, "Atención! " + nombre + " ha pulsado el botón en: " + latitud + " y " + longitud + ".");
-
-            try {
-                startActivity(Intent.createChooser(emailIntent, "Enviar email..."));
-                finish();
-            } catch (android.content.ActivityNotFoundException ex) {
-                Toast.makeText(MainActivity.this,
-                        "No tienes clientes de email instalados.", Toast.LENGTH_SHORT).show();
-            }
-
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Enviar email..."));
+            finish();
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this,
+                    "No tienes clientes de email instalados.", Toast.LENGTH_SHORT).show();
         }
+
+    }
 
     private void makeUseOfNewLocation(Location location) {
 
-       // Toast.makeText(MainActivity.this,
-     //           "LOCALIZADO", Toast.LENGTH_LONG).show();
+        // Toast.makeText(MainActivity.this,
+        //           "LOCALIZADO", Toast.LENGTH_LONG).show();
 
     }
 
 
     // FUNCION AUXILIAR QUE ENVIA EL SMS
-    private void enviarMensaje (String numero, String mensaje){
+    private void enviarMensaje(String numero, String mensaje) {
         try {
             SmsManager sms = SmsManager.getDefault();
-            sms.sendTextMessage(numero,null,mensaje,null,null);
+            sms.sendTextMessage(numero, null, mensaje, null, null);
             Toast.makeText(getApplicationContext(), "Mensaje Enviado.", Toast.LENGTH_LONG).show();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "Mensaje no enviado, datos incorrectos.", Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
     }
 
+
+    // GPS
+    private void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setMainActivity(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+            return;
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
+        //mensaje1.setText("Localización agregada");
+        //mensaje2.setText("");
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationStart();
+                return;
+            }
+        }
+    }
+
+    public void setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address DirCalle = list.get(0);
+                    longitud="Mi direccion es: \n"+ DirCalle.getAddressLine(0);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /* Aqui empieza la Clase Localizacion */
+    public class Localizacion implements LocationListener {
+        MainActivity mainActivity;
+
+        public MainActivity getMainActivity() {
+            return mainActivity;
+        }
+
+        public void setMainActivity(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            // Este metodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
+            // debido a la deteccion de un cambio de ubicacion
+            loc.getLatitude();
+            loc.getLongitude();
+            String Text = "Mi ubicacion actual es: " + "\n Lat = "
+                    + loc.getLatitude() + "\n Long = " + loc.getLongitude();
+            latitud=Text;
+            this.mainActivity.setLocation(loc);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es desactivado
+            Toast.makeText(getApplicationContext(), "GPS desactivado", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es activado
+            Toast.makeText(getApplicationContext(), "GPS activado", Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("debug", "LocationProvider.AVAILABLE");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                    break;
+            }
+        }
+
+    }
 }
+
 
